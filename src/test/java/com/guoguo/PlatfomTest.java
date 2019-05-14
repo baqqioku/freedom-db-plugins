2a81b5834cb16bb07ffa286fe7b40d2a7f361536
576034485@qq.com <bless6984520>
2019-05-14 15:07:55 +0800

    gengxin tijiao

---
+++ src/test/java/com/guoguo/PlatfomTest.java
@@ -0,0 +1,25 @@
+/**
+ * @ClassName: PlatfomTest
+ * @Description:
+ * @author guoguo
+ * @date 2019/5/14
+ * @version V1.0
+ * @since JDK 1.8
+ * <p>
+ * Modification History:
+ * Date         Author          Version            Description
+ * ---------------------------------------------------------
+ * 2019/5/14      guoguo          v1.0.0
+ */
+
+package com.guoguo;
+
+/**
+ * <p></p >
+ *
+ * @author guoguo
+ * @version 1.0, 2019-05-14 15:07 版本及创建日期
+ * @since V1.0
+ */
+public class PlatfomTest {
+}
